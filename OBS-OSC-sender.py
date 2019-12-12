49d55fcc5abb86e10503eefb217604357a403c4c
Ansse Saarimäki <ansse.saarimaki@gmail.com>
2019-12-13 00:04:01 +0200

    Added JSON config file parsing.

--- OBS-OSC-sender.py
+++ OBS-OSC-sender.py
@@ -1,31 +1,130 @@
-import obspython as obs
+import json
 from pathlib import Path
+
+import obspython as obs
+from pythonosc import udp_client
+from pythonosc import osc_message_builder
+from pythonosc import osc_bundle_builder
 
 verbose = False
 config_path = ""
 config_valid = False
+config_dict = {}
+
 signal_handler = None
 
+osc_client = None
+messages_and_bundles = {}
+sources = {}
 
 def script_description():
     return "Sends OSC messages and/or bundles when specific sources are activated.\n\ngithub.com/ansse/OBS-OSC-Sender"
+
+def parse_bundle(bnd_id):
+    success = True
+
+    bnd_items = config_dict["BUNDLES"][bnd_id]
+    if bnd_items is not None:
+        if bnd_id in messages_and_bundles:
+            print("ERROR: Bundle parsing failed. Name clashes with the message \"" + bnd_id + "\".")
+            success = False
+        else:
+            osc_bnd = osc_bundle_builder.OscBundleBuilder(osc_bundle_builder.IMMEDIATELY)
+            for item in bnd_items:
+                if item in messages_and_bundles:
+                    osc_bnd.add_content(messages_and_bundles[item])
+                elif parse_bundle(item):
+                    osc_bnd.add_content(messages_and_bundles[item])
+                else:
+                    print("ERROR: Bundle parsing failed. No message or bundle found with the name \"" + item + "\".")
+                    success = False
+                    break
+            if success:
+                messages_and_bundles[bnd_id] = osc_bnd.build()
+                config_dict["BUNDLES"][bnd_id] = None
+
+    return success
+
+def parse_config():
+    global config_dict
+    global osc_client
+    global messages_and_bundles
+    global sources
+
+    messages_and_bundles = {}
+
+    success = True
+    try:
+        host = config_dict["HOST"]
+        port = config_dict["PORT"]
+        osc_client = udp_client.SimpleUDPClient(host, port)
+    except Exception as e:
+        print("ERROR: Invalid host/port config")
+        print("Failed with exception", e)
+        success = False
+
+    if success:
+        try:
+            messages = config_dict["MESSAGES"]
+            for msg_id in messages:
+                msg = messages[msg_id]
+                addr_id = msg["ADDRESS"]
+                addr = config_dict["ADDRESSES"][addr_id]
+                osc_msg = osc_message_builder.OscMessageBuilder(addr)
+                for arg in msg["ARGUMENTS"]:
+                    osc_msg.add_arg(arg)
+                messages_and_bundles[msg_id] = osc_msg.build()
+
+        except Exception as e:
+            print("ERROR: Message parsing failed")
+            print("Failed with exception", e)
+            success = False
+
+    if success:
+        try:
+            for bnd_id in config_dict["BUNDLES"]:
+                success = parse_bundle(bnd_id)
+                if not success:
+                    break
+
+        except Exception as e:
+            print("ERROR: Bundle parsing failed")
+            print("Failed with exception", e)
+            success = False
+
+    if success:
+        for src_id in config_dict["SOURCES"]:
+            msg_bnd_id = config_dict["SOURCES"][src_id]
+            if msg_bnd_id in messages_and_bundles:
+                sources[src_id] = msg_bnd_id
+            else:
+                print("ERROR: Source parsing failed. No message or bundle found with the name \"" + msg_bnd_id + "\".")
+                success = False
+                break
+
+    return success
 
 
 def load_config():
     global config_path
     global config_valid
+    global config_dict
 
-    config_valid = False
+    config_dict = {}
 
     path = Path(config_path)
     try:
         path = path.resolve()
         print("Loading config file \"" + str(path) + "\"")
         if path.is_file():
-            config_valid = True
+            with open(path, "r") as file:
+                config_dict = json.load(file)
+                #print(config_dict)
+                config_valid = parse_config()
     except Exception as e:
-        print("ERROR: \"" + config_path + "\" is not a valid path.")
+        print("ERROR: \"" + config_path + "\" is not a valid config file.")
         print("Failed with exception", e)
+        config_valid = False
 
     if config_valid:
         print("Config file loaded successfully.")
@@ -69,8 +168,6 @@ def script_update(settings):
         if verbose:
             print("Current config not valid.")
         load_config()
-
-
 
 
 def source_activated(calldata):
